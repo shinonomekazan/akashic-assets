13191ee0c7ef2c82114508c4b316aa7cc17cf410
tsugehara <tsugehara@gmail.com>
2023-04-03 12:55:06 +0900

    合成素材と敵グラフィックを追加 (#18)
    * 合成素材と敵グラフィックを追加
    
    * リンク、日本語にした

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ Akashic Engine向けではありますが、特に利用に制限はないので
 - [背景](./bg/README.md)
 - [キャラクター](./characters/README.md)
 - [マップチップ](./chips/README.md)
+- [敵グラフィック](./enemies/README.md)
 - [ボタン](./buttons/README.md)
 - [汎用素材](./common/README.md)
 
@@ -29,4 +30,4 @@ Akashic Engine向けではありますが、特に利用に制限はないので
 |----|----|----|
 |[株式会社東雲火山](https://shinonomekazan.com/)|本Repository全体のスポンサーです。[汎用素材](./common)、[シルエット](./silhouette)、[背景](./bg/)等を作成しています||
 |本間|[キャラクター](./characters/)、[マップチップ](./chips/)、[ボタン](./buttons/)等を作成しています||
-|[BobTheColored / カラフルなボブ](https://bobthecolored.studio.site/Home)|[キャラクター](./characters/)の[スライム](./characters/slime.png)等を作成しています||
+|[BobTheColored / カラフルなボブ](https://bobthecolored.studio.site/Home)|[キャラクター](./characters/)の[スライム](./characters/slime.png)や合成素材等を作成しています||
---
+++ characters/合成器/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+const imageScript = require("imagescript/v2");
+const imageScriptPngNode = require("imagescript/png/node");
+
+function loadFile(filepath) {
+	const p = path.resolve(filepath);
+	return new Promise((resolve, reject) => {
+		fs.readFile(p, (err, data) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve(data);
+		});
+	});
+}
+
+async function 生成() {
+	const 素材群 = [];
+	素材群.push([
+		"./素体/村人素体_シート.png",
+	], [
+		"./髪/村人髪型1_シート.png",
+		"./髪/村人髪型2_シート.png",
+		"./髪/村人髪型3_シート.png",
+	]);
+	const 選択した素材ファイル = await Promise.all(素材群.map((素材) => 素材[Math.floor(Math.random() * 素材.length)]).map((素材ファイルのパス) => loadFile(素材ファイルのパス)));
+	const 選択した素材 = await Promise.all(
+		選択した素材ファイル.map((素材ファイル) => {
+			const imageLike = imageScriptPngNode.decode(素材ファイル);
+			return new imageScript.Image(imageLike.width, imageLike.height, imageLike.pixels);
+		})
+	);
+	const base = new imageScript.Image(96, 128);
+	選択した素材.forEach((素材, index) => {
+		base.overlay(素材, 0, 0);
+	});
+	const result = await base.encode("png");
+	fs.writeFile(`./result.png`, result, (err) => {
+		if (err) {
+			console.error(err);
+		}
+	});
+}
+
+生成();
