77840b5ad731605c45363282d587d508d33fef43
tsugehara <tsugehara@gmail.com>
2023-05-02 18:15:23 +0900

    合成器拡充 (#19)
    * 合成器拡充
    
    * sample4、帽子あり版に差し替え
    
    * スライム素材を最新版に変更
    
    * 画角修正
    
    * 服と帽子が逆だった

--- characters/合成器/index.js
+++ characters/合成器/index.js
@@ -21,11 +21,27 @@ async function 生成() {
 	素材群.push([
 		"./素体/村人素体_シート.png",
 	], [
-		"./髪/村人髪型1_シート.png",
-		"./髪/村人髪型2_シート.png",
-		"./髪/村人髪型3_シート.png",
+		"./髪/髪1_短髪.png",
+		"./髪/髪2_短髪.png",
+		"./髪/髪3_短髪.png",
+		"./髪/髪4_長髪.png",
+		"./髪/髪5_長髪.png",
+	], [
+		"./服/服1_兵士.png",
+		"./服/服2_兵士.png",
+		"./服/服3_魔法使い.png",
+		"./服/服4_魔法使い.png",
+	], [
+		"./帽子/帽子1_兵士.png",
+		"./帽子/帽子2_兵士.png",
+		"./帽子/帽子3_魔法使い.png",
+		"./帽子/帽子4_魔法使い.png",
 	]);
-	const 選択した素材ファイル = await Promise.all(素材群.map((素材) => 素材[Math.floor(Math.random() * 素材.length)]).map((素材ファイルのパス) => loadFile(素材ファイルのパス)));
+	const 選択した素材ファイル = await Promise.all(素材群.filter((_, index) => {
+		if (index === 0) return true;
+		// 2個目以降はちょっとずつ確率下げる
+		return Math.random() < (0.8 - index * 0.1);
+	}).map((素材) => 素材[Math.floor(Math.random() * 素材.length)]).map((素材ファイルのパス) => loadFile(素材ファイルのパス)));
 	const 選択した素材 = await Promise.all(
 		選択した素材ファイル.map((素材ファイル) => {
 			const imageLike = imageScriptPngNode.decode(素材ファイル);
