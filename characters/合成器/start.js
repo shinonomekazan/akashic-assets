0ab553bc4067f3970cc5485dfc75c8357ca61d94
tsugehara <tsugehara@gmail.com>
2024-02-07 09:51:37 +0900

    prettier入れた

--- characters/合成器/start.js
+++ characters/合成器/start.js
@@ -1,11 +1,11 @@
-const fastify = require('fastify')({logger: true})
-const path = require('node:path')
+const fastify = require("fastify")({ logger: true });
+const path = require("node:path");
 
-fastify.register(require('@fastify/static'), {
-  root: path.join(__dirname),
-  prefix: "/",
+fastify.register(require("@fastify/static"), {
+	root: path.join(__dirname),
+	prefix: "/",
 });
 
 fastify.listen({ port: 3000 }, (err, address) => {
-	if (err) throw err
-})
+	if (err) throw err;
+});
